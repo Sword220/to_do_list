4ca34f9ba3b3cc4ba66baa85d65553244d8ba7a7
Pete Sword <pete.sword@yahoo.com>
2018-05-24 21:22:00 -0600

    created edit

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,5 @@
 class TodosController < ApplicationController
+  
   def index
     @todos = Todo.all
   end
@@ -9,5 +10,20 @@ class TodosController < ApplicationController
 
   def new
     @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.create(todo_params)
+    redirect_to todos_path
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:item)
   end
 end
